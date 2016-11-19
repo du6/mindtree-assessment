2039839246d2158f58371612083c8cbbcc6b2930
Yuelin Du <yuelindu@Yuelins-MacBook-Air.local>
2016-11-19 00:05:41 -0800

    make the node dialog a form with length validators

--- src/main/webapp/src/app/knowledgegraph/add-node-dialog.component.ts
+++ src/main/webapp/src/app/knowledgegraph/add-node-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, destroyPlatform, Optional } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MdDialogRef } from '@angular/material';
 
 @Component({
@@ -6,19 +7,32 @@ import { MdDialogRef } from '@angular/material';
   styleUrls: ['add-node-dialog.component.scss'],
 })
 export class AddNodeDialog {
-  //TODO(du6): bind data with #name.value
-  name: string;
-  description: string;
-  constructor(@Optional() private _dialogRef: MdDialogRef<AddNodeDialog>) { }
+  nodeForm: FormGroup;
+  name: FormControl;
+  description: FormControl;
 
-  cancel() {
+  constructor(
+    @Optional() private _dialogRef: MdDialogRef<AddNodeDialog>,
+    private _fb: FormBuilder,) { 
+    this.name = new FormControl('', Validators.maxLength(100));
+    this.description = new FormControl('', Validators.maxLength(500));
+
+    this.nodeForm = _fb.group({
+      name: this.name,
+      description: this.description,
+    });
+  }
+
+  cancel(event: any) {
     this._dialogRef.close();
   }
 
-  confirm() {
+  confirm(event: any) {
+    // Prevent page reloading
+    event.preventDefault();
     this._dialogRef.close({
-      name: this.name,
-      description: this.description,
+      name: this.name.value,
+      description: this.description.value,
     });
   }
 }
